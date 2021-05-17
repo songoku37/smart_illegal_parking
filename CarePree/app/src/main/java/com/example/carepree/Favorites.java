652ec0d327720dbd1e40631c65ea00f9e3210582
parkjongsu <qkrhwdtn6296@naver.com>
2021-05-17 18:15:27 +0900

    즐겨찾기,설정 페이지 ui디자인

--- CarePree/app/src/main/java/com/example/carepree/Favorites.java
+++ CarePree/app/src/main/java/com/example/carepree/Favorites.java
@@ -2,7 +2,9 @@ package com.example.carepree;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Favorites extends AppCompatActivity {
 
@@ -11,5 +13,23 @@ public class Favorites extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
         // 주차장 목록들 위치 변경 가능하도록
+    }
+    public void enterSearchList(View v){
+        Intent it = new Intent(this,SearchList.class);
+        startActivity(it);
+    }
+    public void enterFavorites(View v){
+        Intent it = new Intent(this,Favorites.class);
+        startActivity(it);
+    }
+
+    public void enterMain(View v){
+        Intent it = new Intent(this,NearParkingLotMain.class);
+        startActivity(it);
+    }
+
+    public void enterSettings(View v){
+        Intent it = new Intent(this,Settings.class);
+        startActivity(it);
     }
 }
