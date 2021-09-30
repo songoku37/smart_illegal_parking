051740c81abed6a5635500b49f10e79e8b032fa2
gustjrdl99 <gustjrdl99@gmail.com>
2021-09-30 21:08:03 +0900

    게시판, 게시글 수정 페이지, 게시글 상세 페이지, 게시글 등록 페이지 추가하였습니다.

---
+++ CarePree/app/src/main/java/com/example/carepree/board.java
@@ -0,0 +1,14 @@
+package com.example.carepree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class board extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+    }
+}
