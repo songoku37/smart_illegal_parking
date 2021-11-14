09b75c8a39204c1992f8cbf78aaaac46b9c530f1
gustjrdl99 <gustjrdl99@gmail.com>
2021-11-14 15:12:35 +0900

    로그인 회원가입 기능 3차 수정본입니다.

--- CarePree/app/src/main/java/com/example/carepree/MainActivity.java
+++ CarePree/app/src/main/java/com/example/carepree/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
